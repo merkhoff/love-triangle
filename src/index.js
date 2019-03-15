3db1bc5782fd31ea489a1bf1ba7c38a1387b1d25
Yury Klopau <neas133@mail.ru>
2019-03-15 10:16:51 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let sum = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let firstTriangle = preferences[i];
+    let secondTriangle = preferences[firstTriangle - 1];
+    let thirdTriangle = preferences[secondTriangle - 1];
+    if (firstTriangle - 1 !== i && thirdTriangle -1 === i) sum++;
+  }
+  return sum / 3;
+}
